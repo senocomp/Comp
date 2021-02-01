0317eb512f11b58dbc91c61687990355726a2105
caamafra <67443144+caamafra@users.noreply.github.com>
2021-02-01 20:47:05 -0300

    Documentação
    Documentação constants.h

--- include/constants.h
+++ include/constants.h
@@ -1,2 +1,8 @@
+/********************************************************************************************
+* Constants.h: Contém as definições de constantes utilizadas em diversos pontos do código.  *
+* Grupo 07: Caroline Mafra, Thais Caliman e Cristian Castro                                 *
+*                                                                                           *
+*********************************************************************************************/
+
 #define MAXIDLEN 32
 #define MAXSTBENTRIES 0x10000
