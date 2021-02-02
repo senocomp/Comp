ed5dd8d9bac4f2e4d5927be6f7dd4087fde855e5
caamafra <67443144+caamafra@users.noreply.github.com>
2021-02-01 21:05:40 -0300

    Documentação

--- lexer.c
+++ lexer.c
@@ -1,3 +1,8 @@
+/********************************************************************************************
+* Lexer.c: Analisador Léxico								    *
+* Grupo 07: Caroline Mafra, Thais Caliman e Cristian Castro                                 *
+*                                                                                           *
+*********************************************************************************************/
 #include<stdio.h>
 #include<stdlib.h>
 #include<ctype.h>
@@ -6,9 +11,11 @@
 #include<keywords.h>
 #include<constants.h>
 
+// Declaração variáveis
 int decimal = 0;
 char lexeme[MAXIDLEN + 1];
 
+// Função que verifica se é um identificador
 int isID(FILE *tape) {
 	int i = 0;
 
@@ -32,6 +39,7 @@ int isID(FILE *tape) {
 	return 0;
 }
 
+// Função que verifica os inteiros
 int isUINT(FILE *tape) {
 	int i = 0;
 	
@@ -56,6 +64,7 @@ int isUINT(FILE *tape) {
 	return 0;
 }
 
+// Função que verifica os númericos
 int isNUM(FILE *tape) {
 	int token = 0, i = 0;
 	int head;
@@ -133,6 +142,7 @@ int isNUM(FILE *tape) {
 	return token;
 }
 
+// Função que verifica os OCTAS
 int isOCT(FILE *tape) {
 	int i = 0;
 	
@@ -155,6 +165,7 @@ int isOCT(FILE *tape) {
 	return 0;
 }
 
+// Função que verifica os HEXAS
 int isHEX(FILE *tape) {
 	int x;
 	int i = 0;
@@ -187,6 +198,7 @@ int isHEX(FILE *tape) {
 	return 0;
 }
 
+// Verifica e pula os espaços
 void skipspaces(FILE *tape) {
     int head;
     
@@ -197,6 +209,7 @@ void skipspaces(FILE *tape) {
     ungetc(head, tape);
 }
 
+// Função responsável por pegar o token e chamar as demais funções
 int gettoken(FILE *source) {
 	int token;
 	
