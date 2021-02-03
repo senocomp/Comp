8055deddac30a5344cd36665cabef2bd0a53fa9c
Cristian <51866369+cristian-95@users.noreply.github.com>
2021-02-03 16:48:38 -0300

    Documentação

--- include/lexer.h
+++ include/lexer.h
@@ -1,5 +1,10 @@
+/********************************************************************************************
+* Lexer.h: Contém variaveis usadas em lexer.c                                                                                 *
+* Grupo 07: Caroline Mafra, Thais Caliman e Cristian Castro                                 *
+*                                                                                           *
+*********************************************************************************************/
 #include<constants.h>
 
-// Declaração variáveis
+// Declaração das variáveis
 int decimal = 0;
 char lexeme[MAXIDLEN + 1];
