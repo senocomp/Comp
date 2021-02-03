857ec74cd313dbfb09dd87637e7afdfb4a2c2f76
Cristian <51866369+cristian-95@users.noreply.github.com>
2021-02-03 16:53:35 -0300

    Documentação

--- include/mybc.h
+++ include/mybc.h
@@ -1,12 +1,13 @@
 /********************************************************************************************
-* Parser.h                                                                                  *
+* mybc.h: Contém declarações de variaveis, e prototipos de funções presentes em mybc.c      *
 * Grupo 07: Caroline Mafra, Thais Caliman e Cristian Castro                                 *
 *                                                                                           *
 *********************************************************************************************/
 
-//Declarações
+// Prototipo das funções:
 void mybc(void);
 int gettoken(FILE *);
 
+//Declaração das variaveis
 int lookahead;
 FILE *source;
